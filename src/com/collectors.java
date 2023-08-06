1557e8836eb299853e43d869e31f6f5e44f55f48
trinadh <trinadhbommineedu@gmail.com>
2023-08-06 08:30:17 +0530

    simple eclipse

---
+++ src/com/collectors.java
@@ -0,0 +1,28 @@
+package com;
+
+public class collectors {
+
+	
+	public void m1() {
+		
+		
+		System.out.println("hello world");
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		collectors c =new collectors();
+		
+		c.m1();
+		
+		
+	}
+	
+	
+	
+	
+}
